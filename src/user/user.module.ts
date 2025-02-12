ad91f3a4cf7c24ea18005e82c205bbdd900cf350
Diones <pereiradiones987@gmail.com>
2025-02-12 16:46:11 -0300

    Circular dependency

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,11 @@
-import { Module} from '@nestjs/common';
+import { forwardRef, Module} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, forwardRef(() => AuthModule)],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
