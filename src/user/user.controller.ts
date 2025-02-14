838bcc472d9f1d583254bd68f0d2b6d893e51fd5
Diones <pereiradiones987@gmail.com>
2025-02-14 14:13:04 -0300

    Proteção Contra Ataque de Força Bruta (Rate Limiting)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseInterceptors, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post, Body, Patch, Param, Delete, ParseIntPipe, UseGuards
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
