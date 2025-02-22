a5943658d2d772f475c6706cc72f9481609706cb
Diones <pereiradiones987@gmail.com>
2025-02-21 22:42:19 -0300

    configuração inicial do swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,6 +14,15 @@ async function bootstrap() {
   app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('API de Autenticação e Authorização com JWT')
+    .setDescription('Esta API implementa emdpoints de autenticação e authorização com JWT e além disso, implementa um sistema de controle de acesso baseado em roles.')
+    .setVersion('1.0')
+    .build();
+  
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
   Logger.log(`Servidor rodando em http://localhost:${process.env.PORT ?? 3000}`);
